11b86f01df0ecb626df87bff2ad1203458d41132
Ronny Blostein <ronnybl1999@gmail.com>
2022-05-14 18:49:55 -0400

    Update schedule_priority.c

--- schedule_priority.c
+++ schedule_priority.c
@@ -5,7 +5,7 @@
 #include "list.h"
 #include "task.h"
 #include "cpu.h"
-//Priority based process scheduling algorithm
+//Priority based process scheduling algorithm (Project 2)
 //Linked list initialization with task in each node
 Task pheadTask ={0,0,0,0};
 struct node head={&pheadTask,0};
